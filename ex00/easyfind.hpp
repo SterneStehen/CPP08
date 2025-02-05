e3e4c92dae4548ae5f677d77b9d434cc10ade77a
Sternestehen <7353718@google.com>
2025-02-05 12:15:42 +0100

    add makefile

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,22 @@
 /*   By: smoreron <smoreron@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 15:20:21 by smoreron          #+#    #+#             */
-/*   Updated: 2025/02/03 15:20:22 by smoreron         ###   ########.fr       */
+/*   Updated: 2025/02/05 12:08:53 by smoreron         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+template <typename T>
+typename T::iterator easyfind(T &v, int i){
+	typename T::iterator res =  std::find(v.begin(), v.end(), i);
+	if(res != v.end()){
+		std::cout << *res << " distance: "<< std::distance(v.begin(), res) << std::endl;
+	}
+	else
+		throw(std::runtime_error  (" element not found "));
+	return res;
+}
+
+#endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: smoreron <smoreron@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 19:06:49 by smoreron          #+#    #+#             */
-/*   Updated: 2025/02/05 11:55:13 by smoreron         ###   ########.fr       */
+/*   Updated: 2025/02/05 12:14:41 by smoreron         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -16,23 +16,17 @@
 #include <string>
 #include <exception>
 #include <list>
-
-
-template <typename T>
-typename T::iterator easyfind(T &v, int i){
-	typename T::iterator res =  std::find(v.begin(), v.end(), i);
-	if(res != v.end()){
-		std::cout << *res << " distance: "<< std::distance(v.begin(), res) << std::endl;
-	}
-	else
-		throw(std::runtime_error  (" element not found "));
-	return res;
-}
+#include "easyfind.hpp"
 
 
 int main(){
-	std::vector<int> v = {3, 5, 6, 55, 66, 72};
-	int i = 66;
+	std::vector<int> v;
+	for (size_t i = 0; i < 20; i++)
+	{
+		v.push_back(i);
+	}
+	
+	int i = 16;
 	try{
 		std::vector<int>::iterator res = easyfind(v, i);
 		if(res != v.end()){
@@ -43,8 +37,12 @@ int main(){
 		std::cout <<"Error "<< i << " "<< e.what() << std::endl;
 	}
 
-	std::list<int> l = {10, 20, 30, 40, 50};
-	int i2 = 30;
+	std::list<int> l;
+	for (size_t i = 0; i < 20; i++)
+	{
+		l.push_back(i);
+	}
+	int i2 = 19;
 	try{
 		std::list<int>::iterator res = easyfind(l, i2);
 		if(res != l.end()){
