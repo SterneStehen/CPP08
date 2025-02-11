bb52d84d761a5f21ad53ccc945841c228d5131dc
Sternestehen <7353718@google.com>
2025-02-11 05:43:18 +0100

    realis MutantStack

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,32 @@
 /*   By: smoreron <smoreron@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 12:18:14 by smoreron          #+#    #+#             */
-/*   Updated: 2025/02/05 12:18:15 by smoreron         ###   ########.fr       */
+/*   Updated: 2025/02/11 05:38:33 by smoreron         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+#include <iterator>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+public:
+	using std::stack<T>::stack;
+	
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+
+	iterator begin() { return std::stack<T>::c.begin(); }
+	iterator end() { return std::stack<T>::c.end(); }
+
+	const_iterator begin() const { return std::stack<T>::c.begin(); }
+	const_iterator end() const { return std::stack<T>::c.end(); }
+
+
+};
+
+#endif 
